1a2cb5e4f20710730e943a9798d7f923fba9eaf2
ynazon1 <yvesnazon2020@u.northwestern.edu>
2015-10-18 07:34:25 -0500

    Trying to get values to write to joint state

--- nodes/planar_robot.py
+++ nodes/planar_robot.py
@@ -3,18 +3,48 @@
 import rospy
 import math
 from sensor_msgs.msg import JointState
-
+from std_msgs.msg import Header
+        
 def planar_robot():
-    pub = rospy.Publisher('joint_states', JointState, queue_size=10)
+    pub = rospy.Publisher('joint_states', JointState, queue_size=1)
     rospy.init_node('planar_robot', anonymous=True)
     rate = rospy.Rate(50) # 50hz
-    use_gui:=true
+    l1 = 1
+    l2 = 1
+    t0 = rospy.get_time() 
+    
     while not rospy.is_shutdown():
-        js = 
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+
+        t1 = rospy.get_time()
+        t3 = t0 - t1
+        t= float(t3)
+        xpos = 0.5*math.cos((2*math.pi*t)/5.0)+1.25
+        ypos = 0.5*math.sin((2*math.pi*t)/5.0)
+
+        
+        xpos = int(xpos)
+        ypos = int(ypos)
+        
+        theta2 = math.acos(((xpos^2)+(ypos^2)-(l1^2)-(l2^2))/(2*l1*l2))
+        phi = math.atan2(ypos,xpos)
+        psi = math.atan2(l2*math.sin(theta2),l1+l2*math.cos(theta2))
+        theta1 = phi - psi
+
+        print t 
+        print xpos
+        print ypos
+
+        theta_input= [theta1,theta2]
+        header= t1
+        name=['joint1','joint2']
+        
+
+        js = JointState() 
+        
+        rospy.loginfo(js)
+        pub.publish(header,name,theta_input,[])
         rate.sleep()
+
 
 if __name__ == '__main__':
     try:
