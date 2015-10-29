6f01e3932c77e7138be81b00b00ef3b41296174d
Jarvis Schultz <schultzjarvis@gmail.com>
2015-10-29 14:47:25 -0500

    Fixed most of the issues that I saw

--- nodes/planar_robot.py
+++ nodes/planar_robot.py
@@ -9,12 +9,13 @@ def planar_robot():
     rospy.init_node('planar_robot', anonymous=True)
     pub = rospy.Publisher('joint_states', JointState, queue_size=1)
     
-    
     l1 = 1
     l2 = 1
     t0 = rospy.get_time() 
     js = JointState()
     
+    rate = rospy.Rate(50) # 50hz
+
     while not rospy.is_shutdown():
         
         t1 = rospy.get_time()
@@ -25,46 +26,21 @@ def planar_robot():
         
         xpos = 0.5*math.cos((2*math.pi*t)/5.0) + 1.25
         ypos = 0.5*math.sin((2*math.pi*t)/5.0)
-
-        
-        #xpos = int(xpos)
-        #ypos = int(ypos)
-
-        #a = l1
-        #b = l2
-        #d = xpos
-        #h = ypos
-        #f = (d**2 + h**2 - a**2 - b**2)/(2*a)
-
-        #theta2 = 2*math.atan2(1*math.sqrt(b**2 - f**2),b + f)
-
-        #m = a + b*math.cos(theta2)
-        #n = b + math.sin(theta2)
-
-        #x1 = (d*m + h*n)/(m**2 + n**2)
-        #y1 = (h - n*x1)/m
-
-        #theta1 = math.atan2(y1,x1)
-
-        #theta21 = 2*math.atan2(math.sqrt(part1/part2))
         
         theta2 = math.acos(((xpos**2)+(ypos**2)-(l1**2)-(l2**2))/(2*l1*l2))
         phi = math.atan2(ypos,xpos)
         psi = math.atan2(l2*math.sin(theta2),(l1+l2*math.cos(theta2)))
         theta1 = phi - psi
 
-        theta_input = [theta1,theta2,theta2]
+        theta_input = [theta1,theta2]
         header = t2
-        name =['joint1','joint2','joint3']
+        name =['joint1','joint2']
 
         js.header.stamp = header
         js.position = theta_input
         js.name = name
 
-        
-        rate = rospy.Rate(50) # 50hz
-
-        rospy.loginfo(js)
+        # rospy.loginfo(js)
         pub.publish(js)
         rate.sleep()
 
--- nodes/planar_robot_listener.py
+++ nodes/planar_robot_listener.py
@@ -2,20 +2,30 @@
 import rospy
 import math
 import tf
-from sensor_msgs.msg import JointState
+import visualization_msgs.msg
 import geometry_msgs.msg
-import visualization_msgs.msg 
 
 if __name__ == '__main__':
     rospy.init_node('planar_robot_listener')
 
     listener = tf.TransformListener()
 
-
-
     end_eff = rospy.Publisher('marker', visualization_msgs.msg.Marker, queue_size=1)
-    ee = rospy.Publisher('stuff', geometry_msgs.msg.Point, queue_size=1)
     rate = rospy.Rate(10.0)
+
+    vm = visualization_msgs.msg.Marker()
+    marker = vm
+    marker.header.frame_id = "base_link"
+    marker.ns = "marker_uno";
+    marker.id = 0;
+    marker.type = 4
+    point_list = []
+    marker.color.a = 1.0 #Don't forget to set the alpha!
+    marker.color.r = 0.0
+    marker.color.g = 1.0
+    marker.color.b = 0.0
+    marker.scale = geometry_msgs.msg.Vector3(0.01, 0.01, 0.01)
+    
     while not rospy.is_shutdown():
         try:
             (trans,rot) = listener.lookupTransform('base_link', 'link3', rospy.Time(0))
@@ -25,34 +35,12 @@ if __name__ == '__main__':
         xtrans = trans[0]
         ytrans = trans[1]
 
-        print xtrans
-        print ytrans
-
-        vm = visualization_msgs.msg.Marker()
-        marker = vm
-        marker.header.frame_id = "base_link"
-        marker.header.stamp = rospy.Time(0);
-        marker.ns = "marker_uno";
-        marker.id = 0;
-        marker.type = 4
-        marker.pose.position.x = xtrans
-        marker.pose.position.y = ytrans
-        marker.pose.position.z = 0
-        marker.point.position.x = xtrans
-        marker.point.position.y = ytrans
-        marker.point.position.z = 0.0
-        marker.color.a = 1.0 #Don't forget to set the alpha!
-        marker.color.r = 0.0
-        marker.color.g = 1.0
-        marker.color.b = 0.0
-        #vm.xtrans.x = xtrans
-        #vm.ytrans.y = ytrans
-        
-        gm = geometry_msgs.msg.Point 
-        gm.x = xtrans
-        gm.y = ytrans
-
-        ee.publish(gm)
+        gm = geometry_msgs.msg.Point(xtrans, ytrans, 0)
+        if len(point_list) > 30:
+            point_list.pop(0)
+        point_list.append(gm)
+        vm.points = point_list
+        vm.header.stamp = rospy.Time.now()
         end_eff.publish(vm)
 
         rate.sleep()
